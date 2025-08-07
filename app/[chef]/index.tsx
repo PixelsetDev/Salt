d04cb5a18584bcf16fd2b294c688d8a6f3de3603
Lewis Milburn <lewiscmilburn@gmail.com>
2025-08-07 13:01:04 +0100

    Update indents

--- app/[chef]/index.tsx
+++ app/[chef]/index.tsx
@@ -20,8 +20,8 @@ export default function App() {
     slug: string;
     title: string;
     author: {
-        name: string;
-        username: string;
+      name: string;
+      username: string;
     }
   }[]>([]);
 
@@ -43,24 +43,24 @@ export default function App() {
     fetch("https://api.ourcookbook.org/recipes?user="+cleanId, { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
-          if (Array.isArray(data.data)) {
-              setRecipes(data.data);
-          } else {
-              setRecipes([]);
-              console.warn("No recipes found:", data.status?.message || "Unknown issue");
-          }
+        if (Array.isArray(data.data)) {
+          setRecipes(data.data);
+        } else {
+          setRecipes([]);
+          console.warn("No recipes found:", data.status?.message || "Unknown issue");
+        }
       })
       .catch((err) => console.error(err));
 
     fetch("https://api.ourcookbook.org/collections?user="+cleanId, { method: "GET" })
       .then((res) => res.json())
       .then((data) => {
-          if (Array.isArray(data.data)) {
-              setCollections(data.data);
-          } else {
-              setCollections([]);
-              console.warn("No collections found:", data.status?.message || "Unknown issue");
-          }
+        if (Array.isArray(data.data)) {
+          setCollections(data.data);
+        } else {
+          setCollections([]);
+          console.warn("No collections found:", data.status?.message || "Unknown issue");
+        }
       })
       .catch((err) => console.error(err));
   }, [cleanId]);
@@ -83,52 +83,52 @@ export default function App() {
       </View>
 
       <View className="p-std grid gap-xl">
-          <View className="grid gap-std">
-              <Text className="h2 font-serif text-center">Recipes</Text>
+        <View className="grid gap-std">
+          <Text className="h2 font-serif text-center">Recipes</Text>
 
-              <View className="grid-2 gap-std">
-                  {recipes.map((recipe) => (
-                      <OLink
-                          href={"/@"+cleanId+"/"+recipe.slug}
-                          key={recipe.slug}
-                          className="btn-np btn-primary flex flex-row space-x-2"
-                      >
-                          <Image
-                              source={{ uri: "https://api.ourcookbook.org/storage/recipes/@"+cleanId+"/"+recipe.slug+".webp" }}
-                              className="h-full rounded-l-md w-20"
-                          />
-                          <View className="grid gap-2 px-4 py-2">
-                              <Text className="font-serif txt-2xl text-white">{recipe.title}</Text>
-                              <OText className="txt-xl text-white">By {recipe.author.name}</OText>
-                          </View>
-                      </OLink>
-                  ))}
-              </View>
+          <View className="grid-2 gap-std">
+            {recipes.map((recipe) => (
+              <OLink
+                href={"/@"+cleanId+"/"+recipe.slug}
+                key={recipe.slug}
+                className="btn-np btn-primary flex flex-row space-x-2"
+              >
+                <Image
+                  source={{ uri: "https://api.ourcookbook.org/storage/recipes/@"+cleanId+"/"+recipe.slug+".webp" }}
+                  className="h-full rounded-l-md w-20"
+                />
+                <View className="grid gap-2 px-4 py-2">
+                  <Text className="font-serif txt-2xl text-white">{recipe.title}</Text>
+                  <OText className="txt-xl text-white">By {recipe.author.name}</OText>
+                </View>
+              </OLink>
+            ))}
           </View>
-          <View className="grid gap-std">
-              <Text className="h2 font-serif text-center">Collections</Text>
+        </View>
+        <View className="grid gap-std">
+          <Text className="h2 font-serif text-center">Collections</Text>
 
-              <View className="grid-2 gap-std">
-                  {collections.map((collection) => (
-                      <OLink
-                          href={"/collections/"+collection.slug}
-                          key={collection.slug}
-                          className="btn-np btn-primary flex flex-row space-x-2"
-                      >
-                          <View className="grid gap-2 px-4 py-2">
-                              <View className="flex flex-row space-x-2">
-                                  <Text className="font-serif txt-2xl text-white">{collection.name}</Text>
-                                  <View className="flex-grow"></View>
-                                  {collection.featured && (
-                                      <OText className="txt-xl text-white">Featured</OText>
-                                  )}
-                              </View>
-                              <OText className="txt-xl text-white">{collection.description}</OText>
-                          </View>
-                      </OLink>
-                  ))}
-              </View>
+          <View className="grid-2 gap-std">
+            {collections.map((collection) => (
+              <OLink
+                href={"/collections/"+collection.slug}
+                key={collection.slug}
+                className="btn-np btn-primary flex flex-row space-x-2"
+              >
+                <View className="grid gap-2 px-4 py-2">
+                  <View className="flex flex-row space-x-2">
+                    <Text className="font-serif txt-2xl text-white">{collection.name}</Text>
+                    <View className="flex-grow"></View>
+                    {collection.featured && (
+                      <OText className="txt-xl text-white">Featured</OText>
+                    )}
+                  </View>
+                  <OText className="txt-xl text-white">{collection.description}</OText>
+                </View>
+              </OLink>
+            ))}
           </View>
+        </View>
       </View>
     </ScrollView>
   );
