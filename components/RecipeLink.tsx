15b50bf3d7d0c1ff1d26174b37f8ca515fa5efb3
Lewis Milburn <lewiscmilburn@gmail.com>
2025-08-07 13:01:32 +0100

    Update

--- components/RecipeLink.tsx
+++ components/RecipeLink.tsx
@@ -2,22 +2,28 @@ import { Image, Text, View } from 'react-native';
 import { OLink, OText } from './Overrides';
 import { ReactNode } from 'react';
 
-export const RecipeLink = ({ recipe, children }: { recipe: object; children: ReactNode }) => {
+export const RecipeLink = ({ recipe, children }: { recipe: { author: { username: string, name: string }, slug:string, title:string }; children: ReactNode }) => {
   return (
     <OLink
-      href={"/@"+recipe.author.username+"/"+recipe.slug}
+      href={'/@' + recipe.author.username + '/' + recipe.slug}
       key={recipe.slug}
-      className="btn-np btn-primary grid gap-2"
-    >
+      className="btn-np btn-primary grid gap-2">
       <Image
-        source={{ uri: "https://api.ourcookbook.org/storage/recipes/@"+recipe.author.username+"/"+recipe.slug+".webp" }}
-        className="rounded-t-md h-40"
+        source={{
+          uri:
+            'https://api.ourcookbook.org/storage/recipes/@' +
+            recipe.author.username +
+            '/' +
+            recipe.slug +
+            '.webp',
+        }}
+        className="h-40 rounded-t-md"
       />
       <View className="grid gap-2 px-4 py-3">
-        <Text className="font-serif txt-2xl text-white">{recipe.title}</Text>
+        <Text className="txt-2xl font-serif text-white">{recipe.title}</Text>
         {children}
         <OText className="txt-xl text-white">By {recipe.author.name}</OText>
       </View>
     </OLink>
-  )
-}
+  );
+};
---
+++ components/Scales.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const Difficulty = ({ currentStep, steps = [] }:{currentStep:number, steps:string[]}) => {
+  return (
+    <View className="flex-row items-center">
+      {steps.map((label : string, index : number) => {
+        const stepNumber = index + 1;
+        const isActive = stepNumber === currentStep;
+
+        return (
+          <React.Fragment key={label}>
+            <View className="items-center">
+              <View
+                className={`rounded-full w-8 h-8 justify-center items-center
+                  ${isActive ? "bg-green" : "bg-green-200"}`}
+              >
+              </View>
+              <Text className="text-xs mt-1 text-center">{label}</Text>
+            </View>
+
+            {index < steps.length - 1 && (
+              <View
+                className={`h-1 flex-1 mx-1 mb-4 bg-green-200`}
+              />
+            )}
+          </React.Fragment>
+        );
+      })}
+    </View>
+  );
+};
+
+export const Stepper = ({ currentStep, steps = [] }: {currentStep:number, steps:string[]}) => {
+  return (
+    <View className="flex-row items-center">
+      {steps.map((label : string, index : number) => {
+        const stepNumber = index + 1;
+        const isCompleted = stepNumber < currentStep;
+        const isActive = stepNumber === currentStep;
+
+        return (
+          <React.Fragment key={label}>
+            <View className="items-center">
+              <View
+                className={`rounded-full w-8 h-8 justify-center items-center
+                  ${isActive ? "bg-green-900" : isCompleted ? "bg-green-600" : "bg-green-200"}`}
+              >
+                <Text className={`font-bold text-sm
+                  ${(isActive || stepNumber < currentStep) ? "text-white" : "text-green-900"}`}>{stepNumber}</Text>
+              </View>
+              <Text className="text-xs mt-1 text-center">{label}</Text>
+            </View>
+
+            {index < steps.length - 1 && (
+              <View
+                className={`h-1 flex-1 mx-1 mb-4
+                  ${stepNumber < currentStep ? "bg-green-600" : "bg-green-200"}`}
+              />
+            )}
+          </React.Fragment>
+        );
+      })}
+    </View>
+  );
+};
